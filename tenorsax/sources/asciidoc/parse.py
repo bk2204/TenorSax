3b3aebcb2026c2db735b45f6b967c9d27a51c6f1
brian m. carlson <sandals@crustytoothpaste.net>
2011-09-02 00:06:03 +0000

    asciidoc: add copyright notice.
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- tenorsax/sources/asciidoc/parse.py
+++ tenorsax/sources/asciidoc/parse.py
@@ -1,3 +1,19 @@
+#-
+# Copyright © 2011 brian m. carlson
+# 
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 2 of the License, dated June 1991.
+# 
+# this program is distributed in the hope that it will be useful,
+# but without any warranty; without even the implied warranty of
+# merchantability or fitness for a particular purpose.  see the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
 import re
 import xml.sax.xmlreader
 
